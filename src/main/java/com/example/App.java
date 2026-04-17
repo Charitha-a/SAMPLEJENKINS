82e21d62922322178e251b1fdc3750cdd309f1c7
Charitha-a <charitha130305@gmail.com>
2026-04-17 23:48:01 +0530

    changess neww

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,12 @@
 package com.example;
-
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+public int add(int a, int b) {
+return a + b;
+}
+public static void main(String[] args) {
+App app = new App();
+int result = app.add(2, 3);
+System.out.println("2 + 3 = " + result);
+System.out.println("Application executed successfully!");
+}
 }
